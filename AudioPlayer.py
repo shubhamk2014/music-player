a50bfc920556ca53f3d4f615818ada216ded54d4
Shubham <shubhamkumbhar2014@gmail.com>
2021-04-27 13:59:11 +0530

    added comments and different functions to code

--- AudioPlayer.py
+++ AudioPlayer.py
@@ -1,10 +1,13 @@
-from tkinter import Tk, Menu, Label, Button, Canvas, Frame, Scale, HORIZONTAL, TRUE, LEFT, BOTH, X
+from tkinter import Tk, Menu, Label, Button, Canvas, Frame, Scale, HORIZONTAL, TRUE, LEFT, BOTH, X, StringVar
+from tkinter.ttk import Scale as s
 from PIL import Image, ImageTk
-from tkinter.ttk import Progressbar
 from tkinter.messagebox import showinfo
 from tkinter.filedialog import askopenfilenames
-import os
 from pygame import mixer
+from random import randint
+from mutagen.mp3 import MP3
+from time import strftime, gmtime
+
 
 # initialization of pygame
 mixer.init()
@@ -14,26 +17,49 @@ root.geometry("530x430")
 root.minsize(530, 430)
 
 
-# music loading and creating playlist
+# music loading
 interpt = 0
 
 
 def openfile(event=None):
-    global interpt
-    global music_files, current_fileInd
+    global music_files, current_fileInd, interpt
     music_files = askopenfilenames(initialdir="E:\\Playlists", defaultextension=".mp3", filetypes=[
                                    ("alltyes", "*.*"), ("musicfiles", "*.mp3")])
-    # print(music_files)
+
     current_fileInd = len(music_files)-1
+    playMusic()
+
+
+def playMusic():
+    global music_files, interpt, current_fileInd
+    # print(current_fileInd)
+
     mixer.music.load(music_files[current_fileInd])
-    mixer.music.play(loops=0)
+    mixer.music.play()
+
     play_btn.config(text="Pause")
     interpt = 1
+    duration()
 
 
 # function for next song
 def next():
     global current_fileInd, interpt
+    # print(current_fileInd)
+
+    current_fileInd = (current_fileInd + 1) % len(music_files)
+    # print(music_files[current_fileInd])
+    mixer.music.load(music_files[current_fileInd])
+    mixer.music.play()
+    play_btn.config(text="Pause")
+    interpt = 0
+
+
+# function for previous song
+def prev():
+    global current_fileInd, interpt
+    # print(current_fileInd)
+
     current_fileInd = (current_fileInd - 1) % len(music_files)
     # print(music_files[current_fileInd])
     mixer.music.load(music_files[current_fileInd])
@@ -41,24 +67,13 @@ def next():
     play_btn.config(text="Pause")
     interpt = 1
 
-
-# function for previous song
-def prev():
-    global current_fileInd, interpt
-    current_fileInd = (current_fileInd + 1) % len(music_files)
-    # print(music_files[current_fileInd])
-    mixer.music.load(music_files[current_fileInd])
-    mixer.music.play()
-    play_btn.config(text="Pause")
-    interpt = 1
+# assigning play and pause to funtionalities to same btn
 
 
 def play_pause():
     global interpt
     if interpt == 0:
-        play()
-        interpt = 1
-        # print("1")
+        playMusic()
 
     elif interpt == 1:
         pause()
@@ -70,6 +85,8 @@ def play_pause():
         interpt = 1
         # print("3")
 
+# function to play music
+
 
 def play():
     global interpt
@@ -78,12 +95,16 @@ def play():
 
     interpt = 1
 
+# function to pause music
+
 
 def pause():
     global interpt
     mixer.music.pause()
     play_btn.config(text="Resume")
     interpt = 2
+
+# function to resume music
 
 
 def resume():
@@ -92,23 +113,38 @@ def resume():
     play_btn.config(text="Pause")
     interpt = 1
 
-
-i = 0
+# function to replay music
 
 
 def replay():
-    pass
+    play()
+
+# function to shuffle music
 
 
 def shuffle():
-    pass
+    global music_files, current_fileInd
+    try:
+
+        current_fileInd = randint(0, len(music_files)-1)
+        # print(current_fileInd)
+        audio_f = music_files[current_fileInd]
+        mixer.music.load(audio_f)
+        mixer.music.play()
+    except Exception:
+        pass
+
+# function to control volume
 
 
 def volume(vol):
     volum = int(vol)/100
+    print(vol)
     mixer.music.set_volume(volum)
     volum = volum*100
     set_img(volum)
+
+# function to set image on volume btn
 
 
 def set_img(volum):
@@ -131,12 +167,16 @@ def set_img(volum):
     vol_btn.config(image=vol_image)
     # print("5")
 
+# function to increase the playback volume
+
 
 def volUp():
     inp = scale.get()
     if inp <= 90:
         inp = int(inp)+10
         scale.set(inp)
+
+# function to decrease the playback volume
 
 
 def volDown():
@@ -146,6 +186,7 @@ def volDown():
         scale.set(inp)
 
 
+# function to mute unmute the song
 muted = 0
 
 
@@ -166,32 +207,65 @@ def mute_unmute(event):
         vol_btn.config(image=vol_image)
         muted = 1
 
+# function to showinfo about player
+
 
 def about():
     showinfo(title='Audio Player',
              message="This is the 'Audio Player' by creator 'Shubham Kumbhar'")
 
 
+# function for moving songProgress in case we want to forward the song
+def set_song_pos(pos):
+    slider_pos = pos.widget.get()
+    mixer.music.set_pos(slider_pos)
+
+
+# function for song lenth and song current position
+
+
+def duration():
+    # global declaration of variables we need in this function
+    global music_files, current_fileInd, current_pos
+    # get_pos returns time in millisec ,converting it to sec by dvidin with 1000
+    current_pos = mixer.music.get_pos()/1000
+    # converting time in the sec into format %M:%S
+    converted_pos = strftime("%M:%S", gmtime(current_pos))
+    # updating label with converted time
+    dur_lbl.config(text=converted_pos)
+    songProgress.set(current_pos)
+    # for getting the position of the song every sec run the function every sec
+    dur_lbl.after(1000, duration)
+
+    # audio file of which we want to find the length
+    audio_f = music_files[current_fileInd]
+    current_audio = MP3(audio_f)
+    audio_len = current_audio.info.length
+    # converting in sec audio lenth to format %m:%s
+    converted_audio_len = strftime("%M:%S", gmtime(audio_len))
+    # finally updating songlen_lbl
+    songlen_lbl.config(text=converted_audio_len)
+    songProgress.config(to=audio_len)
+    print(f"current={current_pos}")
+
+
 menubar = Menu(root)
 
 # filemenu
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(
-    label="Open", accelerator="Ctrl+O", command=openfile)
+
+filemenu.add_command(label="Open", accelerator="Ctrl+O", command=openfile)
 filemenu.bind_all("<Control-o>", openfile)
 filemenu.add_separator()
-filemenu.add_command(
-    label="Exit", accelerator="Alt+F4", command=root.quit)
-# filemenu.bind_all("<Alt-F4>", openfile)
+filemenu.add_command(label="Exit", accelerator="Alt+F4", command=root.quit)
 
 menubar.add_cascade(label="File", menu=filemenu)
 
 # playmenu
 
 playmenu = Menu(menubar, tearoff=0)
-playmenu.add_command(label="Play", accelerator="F5",
-                     command=play)
-# filemenu.bind_all("<F5>", play)
+
+playmenu.add_command(label="Play", accelerator="F5", command=play)
 playmenu.add_command(label="Pause", accelerator="F7", command=pause)
 playmenu.add_command(label="Resume", accelerator="F7", command=resume)
 playmenu.add_separator()
@@ -201,10 +275,9 @@ playmenu.add_separator()
 playmenu.add_command(label="Replay", accelerator="F4", command=replay)
 playmenu.add_command(label="Shuffle", accelerator="F4", command=shuffle)
 playmenu.add_separator()
-playmenu.add_command(label="Volume Up", accelerator="F3",
-                     command=volUp)
-playmenu.add_command(label="Volume Down", accelerator="F2",
-                     command=volDown)
+playmenu.add_command(label="Volume Up", accelerator="F3", command=volUp)
+playmenu.add_command(label="Volume Down", accelerator="F2", command=volDown)
+
 menubar.add_cascade(label="Play", menu=playmenu)
 
 # Helpmenu
@@ -218,31 +291,39 @@ menubar.add_cascade(label="Help", menu=Helpmenu)
 root.config(menu=menubar)
 
 
-filename = "imgs\\music.jpg"
-
 frame1 = Frame(root, width=550, height=300)
 
+filename = "imgs\\music.jpg"
 img = ImageTk.PhotoImage(Image.open(filename))
 Label(frame1, image=img).pack(expand=TRUE, fill=BOTH)
-Label(frame1, text="Playing song",
-      bg="gray", borderwidth=2, fg="white", font="lucida 10 bold").pack(ipadx=10, ipady=10)
 frame1.pack(expand=TRUE, fill=BOTH)
 
 # frame2 is for song progress bar
 frame2 = Frame(root, width=550, height=5)
-progressbar1 = Progressbar(
-    frame2, max=100, mode="determinate").pack(fill=X, padx=10)
-frame2.pack(expand=TRUE, fill=X)
+
+# dur_lbl gives the current position of song i.e. howmuch song is played
+dur_lbl = Label(frame2, text="00:00", bg="gray")
+dur_lbl.pack(side=LEFT, padx=10, ipadx=10, ipady=5)
+
+songProgress = s(frame2, from_=0, to=100,
+                 orient=HORIZONTAL, length=300, value=0)
+songProgress.pack(side=LEFT)
+songProgress.bind("<ButtonRelease>", set_song_pos)
+
+songlen_lbl = Label(frame2, text="00:00", bg="gray")
+songlen_lbl.pack(side=LEFT, padx=10, ipadx=10, ipady=5)
+
+frame2.pack(expand=TRUE)
 
 # can_wid is for all btns
-can_wid = Canvas(frame2, width=550, height=37, bg="gray",
+can_wid = Canvas(root, width=550, height=37, bg="gray",
                  borderwidth=5, relief="raised", highlightbackground="green")
 can_wid.pack()
 
 # rewind_btn replaying song
-rewind_btn = Button(can_wid, text="Rewind",
-                    font="lucida 10 bold", bg="green", fg="white")
-rewind_btn.pack(side=LEFT, padx=10, pady=10)
+replay_btn = Button(can_wid, text="Replay", font="lucida 10 bold",
+                    bg="green", fg="white", command=replay)
+replay_btn.pack(side=LEFT, padx=10, pady=10)
 
 # button for previous song
 prev_btn = Button(can_wid, text="<<", font="lucida 10 bold",
@@ -255,11 +336,11 @@ play_btn = Button(can_wid, text="Play", font="lucida 13 bold",
 play_btn.pack(side=LEFT, ipadx=5, ipady=5, padx=10, pady=10)
 
 # button for next song
-Button(can_wid, text=">>", font="lucida 10 bold", bg="green", fg="white", command=next).pack(
-    side=LEFT, padx=10, pady=10)
+Button(can_wid, text=">>", font="lucida 10 bold", bg="green",
+       fg="white", command=next).pack(side=LEFT, padx=10, pady=10)
 # shuffle_btn
-shuffle_btn = Button(can_wid, text="Shuffle",
-                     font="lucida 10 bold", bg="green", fg="white")
+shuffle_btn = Button(can_wid, text="Shuffle", font="lucida 10 bold",
+                     bg="green", fg="white", command=shuffle)
 shuffle_btn.pack(side=LEFT, padx=10, pady=10)
 
 # volume button and condition for different volume
@@ -270,8 +351,8 @@ vol_btn.pack(side=LEFT, padx=10)
 vol_btn.bind("<Button-1>", mute_unmute)
 # volume progessbar
 
-scale = Scale(can_wid, from_=0, to=100,
-              orient=HORIZONTAL, command=volume, bg="green", showvalue=0, width=10)
+scale = Scale(can_wid, from_=0, to=100, orient=HORIZONTAL,
+              command=volume, bg="green", showvalue=0, width=10)
 scale.pack(side=LEFT, padx=10)
 scale.set(80)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-imgs\\lVol.png
+
+    # get_pos returns time in millisec ,converting it to sec by dvidin with 1000
+    current_pos = mixer.music.get_pos()/1000
+    # converting time in the sec into format %M:%S
+    converted_pos = strftime("%M:%S", gmtime(current_pos))
+    # updating label with converted time
+    dur_lbl.config(text=converted_pos)
+    # for getting the position of the song every sec run the function every sec
+    dur_lbl.after(1000, duration)
